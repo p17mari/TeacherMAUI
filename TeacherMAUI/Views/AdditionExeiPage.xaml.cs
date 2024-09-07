ef07dbd16041603bf4c7015d803cdad570a0b725
p17mari <p17mari@ionio.gr>
2024-09-07 21:00:44 +0300

    added a popup page for when the lesson form is submitted succesfully along with comments explaining code fragments

--- TeacherMAUI/Views/AdditionExeiPage.xaml.cs
+++ TeacherMAUI/Views/AdditionExeiPage.xaml.cs
@@ -11,24 +11,24 @@ namespace TeacherMAUI
             InitializeComponent();
         }
 
-        async void OnLessonClicked(object sender, EventArgs e)
+        async void OnLessonClicked(object sender, EventArgs e)//does the commands in the brackets when onlessonclicked button got clicked
         {
-            if (!string.IsNullOrWhiteSpace(tmimaEntry.Text) && !(startchaperonEntry == null) && !(endchaperonEntry == null) && !(dayEntry == null))
+            if (!string.IsNullOrWhiteSpace(tmimaEntry.Text) && !(startchaperonEntry == null) && !(endchaperonEntry == null) && !(dayEntry == null))//does the code in the brackets if the input is not null
             {
-                TimeSpan timeEnds = endchaperonEntry.Time;
-                TimeSpan timeStarts = startchaperonEntry.Time;
-                string selectedDay = (string)dayEntry.SelectedItem;
+                TimeSpan timeEnds = endchaperonEntry.Time;//saving endchaperonentry in timeEnds
+                TimeSpan timeStarts = startchaperonEntry.Time;//saving startchaperonentry in timeStarts
+                string selectedDay = (string)dayEntry.SelectedItem;//saving dayEntry in selectedDay
 
-                await App.Database.SaveExeiAsync(new Exei
+                await App.Database.SaveExeiAsync(new Exei//calling SaveExei for insertion of codebinded input
                 {
-                    Tmima = tmimaEntry.Text,
-                    Lesson = lessonEntry.Text,
-                    Starts = new DateTime(1, 1, 1, timeStarts.Hours, timeStarts.Minutes, timeStarts.Seconds),
-                    Ends = new DateTime(1, 1, 1, timeEnds.Hours, timeEnds.Minutes, timeEnds.Seconds),
-                    Day = selectedDay
-       
-                });
+                    Tmima = tmimaEntry.Text,//inserting the Tmima the class is being taught
+                    Lesson = lessonEntry.Text,//inserting the Lesson the class is being taught
+                    Starts = new DateTime(1, 1, 1, timeStarts.Hours, timeStarts.Minutes, timeStarts.Seconds),//inserting the starting time the class is being taught
+                    Ends = new DateTime(1, 1, 1, timeEnds.Hours, timeEnds.Minutes, timeEnds.Seconds),//inserting the closing time the class is being taught
+                    Day = selectedDay//inserting the day of the week the class is being taught
 
+                });
+                await DisplayAlert("Success", "Class details were saved", "OK");//pop up that says the form entry was submitted
 
             }
 
