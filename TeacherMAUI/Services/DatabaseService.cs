33e1da26aa4f814e2d9c5281bda23256117085f7
p17mari <p17mari@ionio.gr>
2024-09-07 21:24:59 +0300

    Added delete and update for the Efhmeria table in DatabaseService.cs

--- TeacherMAUI/Services/DatabaseService.cs
+++ TeacherMAUI/Services/DatabaseService.cs
@@ -67,11 +67,23 @@ namespace TeacherMAUI.Services
 
         public Task<int> SaveEfhmeriaAsync(Efhmeria efhmeria) //establishing save for insertion of row in table efhmeria
         {
-            return _database.InsertAsync(efhmeria);
+            if (efhmeria.Id != 0)
+            {
+                return _database.UpdateAsync(efhmeria);
+            }
+            else
+            {
+                return _database.InsertAsync(efhmeria);
+            }
         }
         public Task<List<Exei>> GetExeisAsync()  //establishing getlist for ui feedback for table efhmeria
         {
             return _database.Table<Exei>().ToListAsync();
+        }
+
+        public Task<int> DeleteEfhmeriaAsync(Efhmeria efhmeria)//establishing delete for removing row in the table EEfhmeria
+        {
+            return _database.DeleteAsync(efhmeria);
         }
 
         public Task<int> SaveExeiAsync(Exei exei) //establishing save and update for insertion of row in table efhmeria
@@ -89,6 +101,7 @@ namespace TeacherMAUI.Services
         {
             return _database.DeleteAsync(exei);
         }
+
 
     }
 
